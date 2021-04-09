eba02fd53fec4405d5a9511ec0191b0b1fb8fea5
JP VILLEMIN ~aka~ ZheFrench <jpvillemin@gmail.com>
2021-04-09 11:07:24 +0200

    trigger for guality Control added

---
+++ bash/triggers/trigger_qualityControl.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#################################################################
+#
+#date: April 01, 2021
+#platform: Ubuntu 18.04
+# author: Villemin Jean-Philippe
+# team: Bioinformatique et biologie des systèmes du cancer : J. Colinge 
+# Institute : IRCM
+#
+# trigger_qualityControl.sh
+# Usage :
+#
+# ./trigger_qualityControl.sh 
+#
+# Description :
+#
+# Will called Rscript to execute quality controls.
+#
+#################################################################
+
+
+##################################################################
+############     qualityControl Bash         #####################
+##################################################################
+
+# First Batch Erlotinib 2 cell lines
+Rscript /data/villemin/code/singleCell/R/qualityControl.R -d /data/villemin/data/toulouse/scRNAseqCells-1/CellRanger -a 4006_verte -b 4006_rouge -p TRUE
+
+# Second Batch OsiTipi + Control
+
+Rscript /data/villemin/code/singleCell/R/qualityControl.R -d /data/villemin/data/toulouse/scRNAseqCells-2/CellRanger -a CTL_Vertes -b CTL_Rouges -p FALSE
+Rscript /data/villemin/code/singleCell/R/qualityControl.R -d /data/villemin/data/toulouse/scRNAseqCells-2/CellRanger -a OSI_TIPI_Vertes -b OSI_TIPI_Rouges -p TRUE
+ 
+# PDX Human_CTL_GRCh38 Human_OSI Human_TIPI  Human_OSI_TIPI
+
+Rscript /data/villemin/code/singleCell/R/qualityControl.R -d /data/villemin/data/toulouse/scRNAseqPDX/CellRanger -a Human_OSI -b Human_OSI_TIPI -p TRUE
+Rscript /data/villemin/code/singleCell/R/qualityControl.R -d /data/villemin/data/toulouse/scRNAseqPDX/CellRanger -a Human_TIPI -b Human_OSI_TIPI -p TRUE
+Rscript /data/villemin/code/singleCell/R/qualityControl.R -d /data/villemin/data/toulouse/scRNAseqPDX/CellRanger -a Human_CTL_G -b Human_OSI_TIPI -p TRUE
+ 
