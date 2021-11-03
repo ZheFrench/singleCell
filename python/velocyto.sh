e826a19cc88e11f9d7a71dce29a8b36aa8bed859
root <jpvillemin@gmail.com>
2021-11-03 15:43:25 +0100

    velocyto again

---
+++ python/velocyto.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export HDF5_USE_FILE_LOCKING='FALSE'
+
+#H4006
+#velocyto run10x -m /data/USERS/villemin/data/toulouse/hg38_rmsk.gtf /data/USERS/villemin/data/toulouse/scRNAseqCells-1/CellRanger/4006_rouge /data/USERS/villemin/genome/xenome/10X/GRCh38/genes/genes.gtf
+#velocyto run10x -m /data/USERS/villemin/data/toulouse/hg38_rmsk.gtf /data/USERS/villemin/data/toulouse/scRNAseqCells-1/CellRanger/4006_verte /data/USERS/villemin/genome/xenome/10X/GRCh38/genes/genes.gtf
+
+#velocyto run10x -m /data/USERS/villemin/data/toulouse/hg38_rmsk.gtf /data/USERS/villemin/data/toulouse/scRNAseqCells-2/CellRanger/CTL_Rouges  /data/USERS/villemin/genome/xenome/10X/GRCh38/genes/genes.gtf
+#velocyto run10x -m /data/USERS/villemin/data/toulouse/hg38_rmsk.gtf /data/USERS/villemin/data/toulouse/scRNAseqCells-2/CellRanger/CTL_Vertes  /data/USERS/villemin/genome/xenome/10X/GRCh38/genes/genes.gtf
+
+#velocyto run10x -m /data/USERS/villemin/data/toulouse/hg38_rmsk.gtf /data/USERS/villemin/data/toulouse/scRNAseqCells-2/CellRanger/OSI_TIPI_Vertes  /data/USERS/villemin/genome/xenome/10X/GRCh38/genes/genes.gtf
+#velocyto run10x -m /data/USERS/villemin/data/toulouse/hg38_rmsk.gtf /data/USERS/villemin/data/toulouse/scRNAseqCells-2/CellRanger/OSI_TIPI_Rouges  /data/USERS/villemin/genome/xenome/10X/GRCh38/genes/genes.gtf
+
+# PDX
+#velocyto run10x -m /data/USERS/villemin/data/toulouse/hg38_rmsk.gtf /data/USERS/villemin/data/toulouse/scRNAseqPDX/CellRanger/Human_CTL_GRCh38 /data/USERS/villemin/genome/xenome/10X/GRCh38/genes/genes.gtf
+#velocyto run10x -m /data/USERS/villemin/data/toulouse/hg38_rmsk.gtf /data/USERS/villemin/data/toulouse/scRNAseqPDX/CellRanger/Human_OSI_TIPI /data/USERS/villemin/genome/xenome/10X/GRCh38/genes/genes.gtf
+#velocyto run10x -m /data/USERS/villemin/data/toulouse/hg38_rmsk.gtf /data/USERS/villemin/data/toulouse/scRNAseqPDX/CellRanger/Human_OSI /data/USERS/villemin/genome/xenome/10X/GRCh38/genes/genes.gtf
+#velocyto run10x -m /data/USERS/villemin/data/toulouse/hg38_rmsk.gtf /data/USERS/villemin/data/toulouse/scRNAseqPDX/CellRanger/Human_TIPI /data/USERS/villemin/genome/xenome/10X/GRCh38/genes/genes.gtf
+
+
+
+
+
